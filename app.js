156e818b0e732acd8981f3149ce7c1747f6aa09e
otavioalbucosta <otavio-albucosta@hotmail.com>
2019-12-11 16:10:52 -0300

    bugfix

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var roundsRouter = require('./routes/rounds');
 var hintsRouter = require('./routes/hints');
 var scoresRouter = require('./routes/scores');
 var app = express();
+app.use(allowCrossDomain)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
