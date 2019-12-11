f30d44feaff0173112d6a13702d24de6cd5af2f2
otavioalbucosta <otavio-albucosta@hotmail.com>
2019-12-11 16:18:32 -0300

    bugfix try

--- app.js
+++ app.js
@@ -9,14 +9,11 @@ var teamsRouter = require('./routes/teams');
 var roundsRouter = require('./routes/rounds');
 var hintsRouter = require('./routes/hints');
 var scoresRouter = require('./routes/scores');
+var cors = require('cors')
+
 var app = express();
-app.use(allowCrossDomain)
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-  });
+
+  app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
